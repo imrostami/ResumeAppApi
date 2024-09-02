8f8690e73bfaf5c6175ed7f66f2473e29ec74d83
Mohamamd Rostami <sapurtcomputer30@gmail.com>
2024-09-02 07:47:51 -0700

    fix : remove picture alt from `CreateInformationCommand`

--- ResumeAppApi.Application/Informations/Commands/CreateInformation/CreateInformationCommand.cs
+++ ResumeAppApi.Application/Informations/Commands/CreateInformation/CreateInformationCommand.cs
@@ -10,7 +10,6 @@ namespace ResumeAppApi.Application.Informations.Commands.CreateInformation
 		public string LastName { get; set; } = default!;
 		public string About { get; set; } = default!;
 		public string Address { get; set; } = default!;
-		public string PictureAlt { get; set; } = default!;
 		public string Birthday { get; set; } = default!;
 		public string Languages { get; set; } = default!;
 		public string Nationality { get; set; } = default!;
