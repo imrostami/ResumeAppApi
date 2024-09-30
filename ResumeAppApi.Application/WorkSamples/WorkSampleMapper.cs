fe2e3af0643a24d75d0e9375363b28ff8c3ae6f3
Mohamamd Rostami <sapurtcomputer30@gmail.com>
2024-09-30 06:59:30 -0700

    fix : UpdateWorkSample Mapper fixed

--- ResumeAppApi.Application/WorkSamples/WorkSampleMapper.cs
+++ ResumeAppApi.Application/WorkSamples/WorkSampleMapper.cs
@@ -1,6 +1,7 @@
 ﻿using ResumeAppApi.Application.WorkSampleCategories.Commands.UpdateWorkSampleCategory;
 using ResumeAppApi.Application.WorkSampleCategories.Dtos;
 using ResumeAppApi.Application.WorkSamples.Commands.CreateWorkSample;
+using ResumeAppApi.Application.WorkSamples.Commands.UpdateWorkSample;
 using ResumeAppApi.Application.WorkSamples.Dtos;
 using ResumeAppApi.Domain.Entities.WorkSampleAgg;
 
@@ -8,19 +9,22 @@ namespace ResumeAppApi.Application.WorkSamples;
 
 public class WorkSampleMapper : Profile
 {
-    public WorkSampleMapper()
-    {
-        CreateMap<WorkSample, WorkSampleDto>()
-            .ForMember(x => x.Category, obj => obj.MapFrom(map => new WorkSampleCategoryDto()
-            {
-                Name = map.WorkSampleCategory.CategoryName,
-                Id = map.WorkSampleCategory.CategoryId,
-            }))
-            .ForMember(x=>x.Id,obj=>obj.MapFrom(map=>
-            map.SampleId)).ReverseMap();
+	public WorkSampleMapper()
+	{
+		CreateMap<WorkSample, WorkSampleDto>()
+			.ForMember(x => x.Category, obj => obj.MapFrom(map => new WorkSampleCategoryDto()
+			{
+				Name = map.WorkSampleCategory.CategoryName,
+				Id = map.WorkSampleCategory.CategoryId,
+			}))
+			.ForMember(x => x.Id, obj => obj.MapFrom(map =>
+			map.SampleId)).ReverseMap();
 
-        CreateMap<CreateWorkSampleCommand, WorkSample>().ReverseMap();
-        CreateMap<UpdateWorkSampleCategoryCommand, WorkSample>().ReverseMap();
-            
-    }
+		CreateMap<CreateWorkSampleCommand, WorkSample>().ReverseMap();
+
+		CreateMap<UpdateWorkSampleCommand, WorkSample>()
+			.ForMember(x => x.SampleId, obj => obj.MapFrom(map =>
+			map.Id)).ReverseMap();
+
+	}
 }
