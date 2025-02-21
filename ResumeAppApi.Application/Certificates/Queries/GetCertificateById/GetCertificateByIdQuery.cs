84c7d0d8d0c5219f3da68b53f6f7fa87b166eaac
Mohamamd Rostami <sapurtcomputer30@gmail.com>
2025-02-21 12:41:56 -0800

    fix : remove constrctor from GetCertificateByIdQuery to fix This Error : InvalidOperationException: Could not create an instance of type 'ResumeAppApi.Application.Certificates.Queries.GetCertificateById.GetCertificateByIdQuery'. Model bound complex types must not be abstract or value types and must have a parameterless constructor. Record types must have a single primary constructor. Alternatively, give the 'query' parameter a non-null default value.

--- ResumeAppApi.Application/Certificates/Queries/GetCertificateById/GetCertificateByIdQuery.cs
+++ ResumeAppApi.Application/Certificates/Queries/GetCertificateById/GetCertificateByIdQuery.cs
@@ -4,10 +4,5 @@ namespace ResumeAppApi.Application.Certificates.Queries.GetCertificateById;
 
 public class GetCertificateByIdQuery : IRequest<CertificateDto>
 {
-	public GetCertificateByIdQuery(int id)
-	{
-		Id = id;
-	}
-
 	public int Id { get; set; }
 }
